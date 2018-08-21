4eba582689d4b6e8b2ddecfae90ee7610c9b653a
derplayer <derplayer@outlook.com>
2018-08-22 00:55:06 +0200

    * Dirty base commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ShenmueHDTools
 Experimental tools for the Shenmue HD release
+
+Pretty much work in progress right now
---
+++ ShenmueHDTools/tacContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShenmueHDTools
+{
+    /* The archive container/.tac(?) */
+    public class tacItem
+    {
+        public byte[] startPointer = new byte[4];
+        public byte[] sizeData = new byte[4];
+        public byte[] unknownHash = new byte[12]; //xref with exe rdata?
+    }
+}
